f61b64b0b14f3afb2b96917d70a7ab857d70d645
DESKTOP-G959D6Q\Greg <aramageddonbty@aol.com>
2017-10-19 02:04:17 -0700

    add a score label, added game over text, and add retry button.

---
+++ Assets/scripts/Movement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    private Rigidbody2D objectRigidbody;
+    public float speed;
+
+    // Use this for initialization
+    void OnEnable()
+    {
+        objectRigidbody = transform.GetComponent<Rigidbody2D>();
+        objectRigidbody.velocity = transform.up * speed;
+    }
+}
