35aa21e7b2711cf0c862b9aaa701629f62a57342
DESKTOP-G959D6Q\Greg <aramageddonbty@aol.com>
2017-10-27 16:41:11 -0700

    fixed enemy hit detection everything works fine just need to make it look more 8-bit

---
+++ Assets/scripts/EnemyDroneController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyDroneController : MonoBehaviour {
+
+	public GameObject powerUp;
+	public GameObject explosion;
+	public GameObject smallEnemyBullet;
+	public float minReloadTime = 1.0f;
+	public float maxReloadTime = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+    StartCoroutine("Shoot");
+	}
+
+  IEnumerator Shoot() {
+    yield return new WaitForSeconds((Random.Range(minReloadTime, maxReloadTime)));
+    while (true) {
+		  Instantiate(smallEnemyBullet, gameObject.transform.position, gameObject.transform.rotation);
+      yield return new WaitForSeconds((Random.Range(minReloadTime, maxReloadTime)));
+    }
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		if (other.gameObject.tag == "Boundary" && other.gameObject.name != "Top Boundary") {
+      gameObject.SetActive(false);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.tag == "Player Bullet") {
+      GameController.SharedInstance.IncrementScore(100);
+			float randomNumber = Random.Range(0.0f, 10.0f);
+
+			Instantiate(explosion, gameObject.transform.position, gameObject.transform.rotation);
+      other.gameObject.SetActive(false);
+      gameObject.SetActive(false);
+		}
+	}
+}
