f23f081e3143c987705a47004d1e2f14578aa93c
DESKTOP-G959D6Q\Greg <aramageddonbty@aol.com>
2017-11-25 20:34:01 -0700

    a lot of new things added: 2 new levels, timer, load level, boss level added, need to work on scripts for everything

---
+++ Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadLevel : MonoBehaviour {
+    public GameObject enemyType1;
+    public GameObject enemyType2;
+    public GameObject enemyType3;
+    public GameObject enemyType4;
+    public GameObject enemyType5;
+}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
